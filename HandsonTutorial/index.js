a1221b625462b0a943e9c30476370e85d60b284a
ValleJulien <j.valle.julien@gmail.com>
2016-02-24 12:23:32 +0100

    add script to run apache server

--- HandsonTutorial/index.js
+++ HandsonTutorial/index.js
@@ -1,17 +1,30 @@
-// Require Node Modules
+//Require Node Modules
 // var util = require('util');
 
-// in order to watch all util method
+//in order to watch all util method
 // console.log(util);
 
-// Using util methods
+//Using util methods
 // function greet(what) {
 //     util.log('Hello', what + '\n');
 // }
 // greet('Node');
 // greet('NPM');
 
-// Require Local Modules, anonymous fucntion named by a var
-var greet = require('./greet');
-greet('Node');
-greet('NPM');
+//Require Local Modules, anonymous fucntion named by a var
+// var greet = require('./greet');
+// greet('Node');
+// greet('NPM');
+
+//Create an apache server response without the server treat a request
+var http = require("http"),
+    port = 1881;
+    var server = http.createServer(function(request, response) {
+      response.writeHeader(200, {
+        "Content-Type": "text/plain"
+      });
+      response.write("Hello HTTP!");
+      response.end();
+    });
+server.listen(port);
+console.log("Server Running on " + port + ".\nLaunch http://localhost:" + port);
